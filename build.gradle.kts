77cd48b3be544760a29bd3c9fd2aa0f070adfdcf
Francisco <frankwither991000@gmail.com>
2026-03-31 14:25:24 -0700

    Add initial build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// build.gradle.kts en la raíz del proyecto
+
+plugins {
+    // Plugin de Android y Kotlin se aplican en los módulos, no aquí
+    // Aquí puedes aplicar plugins globales si los necesitas
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath(kotlin("gradle-plugin", version = "1.9.0"))
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
