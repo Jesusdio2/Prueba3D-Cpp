37173bf02e40de8291c1f577145eddbf0b75a1ed
Francisco <frankwither991000@gmail.com>
2026-04-05 14:28:49 -0700

    Crear core/game.h

---
+++ core/game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef _WIN32
+    #define EXPORT_API __declspec(dllexport)
+#else
+    #define EXPORT_API
+#endif
+
+extern "C" {
+    EXPORT_API void InitGame3D();
+    EXPORT_API void UpdateGame3D(float deltaTime);
+    EXPORT_API void ShutdownGame3D();
+}
